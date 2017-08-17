e09a2f1ae0815fe8cf1d293b5e464da9a95e95cb
bibekjyotinath <bibekjyotinath92@gmail.com>
2017-08-18 01:41:34 +0530

    Core JAVA Assignment1.1
    Core JAVA Assignment1.1

---
+++ Assignment1.1/src/HelloWorld.java
@@ -0,0 +1,9 @@
+
+public class HelloWorld {     //main class 
+
+	public static void main(String[] args) {   //main method
+		
+		System.out.println("Hello World");	//printing Hello World in Console
+	}
+
+}
